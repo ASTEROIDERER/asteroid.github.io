1bddaf0db8317f176d537074a5b787399cde2d75
番薯小草卷 <2980432018@qq.com>
2025-03-06 21:40:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -49,8 +49,8 @@ noButton.addEventListener("click", function () {
   }
 
   // 图片变化（前 5 次变化）
-  if (clickCount === 1) mainImage.src = "images/shocked.png"; // 震惊
-  if (clickCount === 2) mainImage.src = "images/think.png"; // 思考
+  if (clickCount === 1) mainImage.src = "images/bc964a324a5613e729664feb40af572.png"; // 震惊
+  if (clickCount === 2) mainImage.src = "images/bc964a324a5613e729664feb40af572.png"; // 思考
   if (clickCount === 3) mainImage.src = "images/angry.png"; // 生气
   if (clickCount === 4) mainImage.src = "images/6f1f4394d23d9aa2469f7ef6f21277a.png"; // 哭
   if (clickCount >= 5) mainImage.src = "images/crying.png"; // 之后一直是哭
