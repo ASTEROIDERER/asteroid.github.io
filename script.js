75934fdf8c93adbeaca3285765ec4cfa8187b6d6
番薯小草卷 <2980432018@qq.com>
2025-03-09 00:15:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ noButton.addEventListener("click", function () {
 
   // 图片变化（前 5 次变化）
   if (clickCount === 1) mainImage.src = "images/震惊.png"; // 震惊
-  if (clickCount === 2) mainImage.src = "images/不要.png; // 思考
+  if (clickCount === 2) mainImage.src = "images/不要.png"; // 思考
   if (clickCount === 3) mainImage.src = "images/瘪嘴.png"; // 生气
   if (clickCount === 4) mainImage.src = "images/一滴泪.png"; // 哭
   if (clickCount >= 5) mainImage.src = "images/哭哭.png"; // 之后一直是哭
